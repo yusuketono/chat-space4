f07ad85f9ebcc84c8617586e8d8d14a63c289107
yusuketono <yusuke.tono@di-v.co.jp>
2019-11-12 21:25:02 +0900

    group

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,36 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path,notice: 'グループを編集しました'
+      # 本来下記コードを記載するらしいのだが、仮置きで別コードに記載
+      # redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
+end
+
+def set_group
+  @group = Group.find(params[:id])
+end
